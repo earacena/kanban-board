70aa903ce9a662fac903e76d00fe468e2150abeb
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-03-21 19:30:52 -0400

    Add drag and drop context to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { DndProvider } from 'react-dnd';
+import Container from './Container';
 
 function App() {
   return (
     <div className="App">
-      Test
+      <DndProvider backend={HTML5Backend}>
+        <Container />
+      </DndProvider>
     </div>
   );
 }
