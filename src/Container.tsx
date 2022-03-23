7381f0c6616eaf2bd27674b4f7399fadd80b303b
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-03-23 16:29:03 -0400

    Restructure Container component

--- src/Container.tsx
+++ src/Container.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { CSSProperties, useState } from 'react';
 import Card from './Card';
-import Column from './Column';
+import Column, { Columns } from './Column';
+import { Static as RtStatic } from 'runtypes';
+
+const ContainerStyle: CSSProperties = {
+  display: 'flex',
+  flexDirection: 'column',
+  overflow: 'auto',
+  clear: 'both',
+};
+
+const ButtonStyle: CSSProperties = {
+  width: '4rem',
+};
 
 function Container() {
+  const [columns, setColumns] = useState<RtStatic<typeof Columns>>([]);
+
   return (
     <div>
-      <div style={{ overflow: 'hidden', clear: 'both' }}>
-        <Column />
-      </div>
-      <div style={{ overflow: 'hidden', clear: 'both' }}>
-        <Card label="Card 1" />
-        <Card label="Card 2" />
-        <Card label="Card 3" />
+      <div style={ContainerStyle}>
+        {columns.map((column) => (
+          <Column key={column.id} label={column.label} />
+        ))}
+        <button style={ButtonStyle}>Add Column</button>
       </div>
     </div>
   );
