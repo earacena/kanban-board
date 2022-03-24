9a9ab5fdaf4c414c07e1bd791be9bbb047d98355
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-03-24 13:59:10 -0400

    Add columns state reducer to store configuration

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cardsReducer from './cards.slice';
+import columnsReducer from './columns.slice';
 
 const store = configureStore({
   reducer: {
     cards: cardsReducer,
+    columns: columnsReducer,
   },
 });
 
