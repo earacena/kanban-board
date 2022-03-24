7c94ed3d3a6f04d80fd65d607f0adb9100a92a16
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-03-24 15:30:28 -0400

    Implement adding a column to column state

--- src/columns.slice.ts
+++ src/columns.slice.ts
@@ -2,19 +2,36 @@ import { createSlice } from '@reduxjs/toolkit';
 import { Static as RtStatic } from 'runtypes';
 import { Columns } from './Column';
 
-const initialState: RtStatic<typeof Columns> = [];
+interface ColumnsState {
+  highestId: number,
+  allColumns: RtStatic<typeof Columns>,
+}
+
+const initialState: ColumnsState = {
+  highestId: 0,
+  allColumns: [],
+};
 
 const columnsSlice = createSlice({
   name: 'columns',
   initialState,
   reducers: {
     setColumns: (state, action) => action.payload,
+    addColumn: (state, action) => ({
+      ...state,
+      highestId: state.highestId + 1,
+      allColumns: state.allColumns.concat({
+        id: state.highestId + 1,
+        label: action.payload.label,
+      }),
+    }),
     resetColumns: () => initialState,
   },
 });
 
 export const {
   setColumns,
+  addColumn,
   resetColumns,
 } = columnsSlice.actions;
 
