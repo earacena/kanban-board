6a8bec31f37c4eaa9139919ca85423f83ec9b835
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-03-24 15:32:21 -0400

    Implement adding a card to cards state

--- src/cards.slice.ts
+++ src/cards.slice.ts
@@ -2,19 +2,37 @@ import { createSlice } from '@reduxjs/toolkit';
 import { Static as RtStatic } from 'runtypes';
 import { Cards } from './Card';
 
-const initialState: RtStatic<typeof Cards> = [];
+interface CardsState {
+  highestId: number,
+  allCards: RtStatic<typeof Cards>,
+}
+
+const initialState: CardsState = {
+  highestId: 0,
+  allCards: [],
+};
 
 const cardsSlice = createSlice({
   name: 'cards',
   initialState,
   reducers: {
     setCards: (state, action) => action.payload,
+    addCard: (state, action) => ({
+      ...state,
+      highestId: state.highestId + 1,
+      allCards: state.allCards.concat({
+        id: state.highestId + 1,
+        columnId: action.payload.columnId,
+        label: action.payload.label,
+      }),
+    }),
     resetCards: () => initialState,
   },
 });
 
 export const {
   setCards,
+  addCard,
   resetCards,
 } = cardsSlice.actions;
 
