3a1244fa7965136d97a0cc215337b30f96f0b1a1
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-03-27 18:42:08 -0400

    Implement DragOverlay component

--- src/Container.tsx
+++ src/Container.tsx
@@ -1,4 +1,5 @@
 import React, { CSSProperties } from 'react';
+import { DragOverlay } from '@dnd-kit/core';
 import Column from './Column';
 import { useAppDispatch, useAppSelector } from './hooks';
 import { addColumn } from './columns.slice';
@@ -19,17 +20,42 @@ const ButtonStyle: CSSProperties = {
 function Container() {
   const columns = useAppSelector((state) => state.columns.allColumns);
   const dispatch = useAppDispatch();
-
+  const activeCardId = useAppSelector((state) => state.cards.activeCardId);
+  const activeCard = useAppSelector(
+    (state) => state.cards.allCards.find((c) => c.id === activeCardId),
+  );
   const handleAddColumn = () => dispatch(addColumn({ label: 'Column' }));
 
   return (
-    <div>
-      <div style={ContainerStyle}>
-        {columns.map((column) => (
-          <Column key={column.id} id={column.id} label={column.label} />
-        ))}
-        <button type="button" style={ButtonStyle} onClick={handleAddColumn}>Add Column</button>
-      </div>
+    <div style={ContainerStyle}>
+      {columns.map((column) => (
+        <Column key={column.id} id={column.id} label={column.label} />
+      ))}
+
+      <DragOverlay
+        style={{
+          backgroundColor: 'lightgrey',
+          borderRadius: '8px',
+          boxShadow: '0px 3px 10px rgb(0, 0, 0, 0.5)',
+        }}
+      >
+        {activeCardId
+          && activeCard
+          && (
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+              }}
+            >
+              {`${activeCard.id} - ${activeCard.label}`}
+            </div>
+          )}
+      </DragOverlay>
+
+      <button type="button" style={ButtonStyle} onClick={handleAddColumn}>
+        Add Column
+      </button>
     </div>
   );
 }
