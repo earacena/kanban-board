8a1e4c8feba4fc8bd372bb57a2a79188df114be1
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-03-27 18:40:05 -0400

    Implement handler for DragStartEvent

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
-import { DndContext, DragEndEvent } from '@dnd-kit/core';
-import { setCardColumnId } from './cards.slice';
+import { DndContext, DragEndEvent, DragStartEvent } from '@dnd-kit/core';
+import { setActiveCardId, setCardColumnId, resetActiveCardId } from './cards.slice';
 import Container from './Container';
 import { useAppDispatch } from './hooks';
 
 function App() {
   const dispatch = useAppDispatch();
+
+  const handleDragStart = (event: DragStartEvent) => {
+    dispatch(setActiveCardId(Number(event.active.id)));
+  };
+
   const handleDragEnd = (event: DragEndEvent) => {
     const { active, over } = event;
     if (over) {
       dispatch(setCardColumnId({ id: Number(active.id), newColumnId: Number(over.id) }));
     }
+
+    dispatch(resetActiveCardId());
   };
 
   return (
     <div className="App">
-      <DndContext onDragEnd={handleDragEnd}>
+      <DndContext onDragStart={handleDragStart} onDragEnd={handleDragEnd}>
         <Container />
       </DndContext>
     </div>
