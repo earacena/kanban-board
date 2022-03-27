046368272304dd609bc052d29d28d5a7e19ad8fe
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-03-26 22:34:03 -0400

    Add drag/drop context and implement drop handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { DndContext, DragEndEvent } from '@dnd-kit/core';
+import { setCardColumnId } from './cards.slice';
 import Container from './Container';
+import { useAppDispatch } from './hooks';
 
 function App() {
+  const dispatch = useAppDispatch();
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event;
+    if (over) {
+      dispatch(setCardColumnId({ id: Number(active.id), newColumnId: Number(over.id) }));
+    }
+  };
+
   return (
     <div className="App">
-      <Container />
+      <DndContext onDragEnd={handleDragEnd}>
+        <Container />
+      </DndContext>
     </div>
   );
 }
