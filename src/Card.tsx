d7bc759997c1e83dccd5875f6341621b37f08e8a
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-03-29 19:58:07 -0400

    Add body field to Card and a separator line

--- src/Card.tsx
+++ src/Card.tsx
@@ -15,16 +15,23 @@ interface CardProps {
   id: string;
   label: string;
   columnId: string,
+  body: string,
 }
 
 function Card({
   id,
   label,
   columnId,
+  body,
 }: CardProps) {
+  console.log(`Card ${id} ${label} ${columnId}`);
   return (
     <div>
-      {`[${columnId}] ${label} - ${id}`}
+      <div style={{ borderBottom: '1px grey solid' }}>
+        {label}
+      </div>
+
+      {body}
     </div>
   );
 }
