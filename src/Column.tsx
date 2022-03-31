a1cff4b04d8ffdb1751bd445c01aa0c4ef816fa8
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-03-30 23:48:36 -0400

    Modify Column style and reposition buttons

--- src/Column.tsx
+++ src/Column.tsx
@@ -14,24 +14,32 @@ import ColumnEditForm from './ColumnEditForm';
 const style: CSSProperties = {
   display: 'flex',
   flexDirection: 'column',
-  border: '1px red solid',
+  border: '2px lightgrey solid',
+  borderRadius: '5px',
+  backgroundColor: '#EEEEEE',
   height: '100%',
-  minHeight: '15rem',
+  minHeight: '50rem',
   minWidth: '20rem',
   marginRight: '1.5rem',
   marginBottom: '1.5rem',
   color: 'black',
   padding: '1rem',
+  paddingLeft: '0.1rem',
+  paddingRight: '0.1rem',
   textAlign: 'center',
   fontSize: '1rem',
   lineHeight: 'normal',
   float: 'left',
+  boxShadow: '0px 3px 10px rgb(0, 0, 0, 0.4)',
 };
 
 const cardStyle: CSSProperties = {
+  display: 'flex',
+  flexDirection: 'row',
   border: '1px lightgrey solid',
   backgroundColor: 'white',
   padding: '1rem',
+  margin: '0.25rem',
   borderRadius: '5px',
   boxShadow: '0px 3px 10px rgb(0, 0, 0, 0.2)',
   borderLeft: '3px red solid',
@@ -52,23 +60,30 @@ function Column({ id, label }: ColumnProps) {
 
   return (
     <Droppable id={id} key={id} style={style}>
-      {!beingEdited && label}
-      {beingEdited && (
-        <ColumnEditForm
-          id={id}
-          prevLabel={label}
-          beingEdited={beingEdited}
-          setBeingEdited={setBeingEdited}
-        />
+      {!beingEdited && (
+        <span>
+          {label}
+          <button type="button" onClick={() => setBeingEdited(!beingEdited)}>Edit</button>
+        </span>
       )}
-      {!beingEdited && <button type="button" onClick={() => setBeingEdited(!beingEdited)}>Edit</button>}
+      {beingEdited && (
+        <span>
+          <ColumnEditForm
+            id={id}
+            prevLabel={label}
+            beingEdited={beingEdited}
+            setBeingEdited={setBeingEdited}
+          />
+          <button type="button" onClick={() => setBeingEdited(!beingEdited)}>Cancel</button>
+        </span>
+      )}
       <SortableContext items={cardIds} strategy={verticalListSortingStrategy}>
         {cardsInThisColumn.map((card) => (
           <SortableItem key={card.id} id={card.id} style={{ ...cardStyle, borderLeft: ` 3px${card.color} solid` }}>
             <Card
               key={card.id}
               id={card.id}
-              label={card.label}
+              brief={card.brief}
               columnId={card.columnId}
               body={card.body}
             />
@@ -76,7 +91,7 @@ function Column({ id, label }: ColumnProps) {
         ))}
       </SortableContext>
       {!formVisible && (
-        <button type="button" onClick={() => setFormVisible(!formVisible)}>
+        <button style={{ marginTop: 'auto' }} type="button" onClick={() => setFormVisible(!formVisible)}>
           Add Card
         </button>
       )}
