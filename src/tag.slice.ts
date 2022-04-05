76d1f73280f3854cff5b09d2b9c79e6dc6b06281
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-04-04 22:45:49 -0400

    Implement tags state handling

---
+++ src/tag.slice.ts
@@ -0,0 +1,40 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { Static as RtStatic } from 'runtypes';
+import { Tags } from './tag.types';
+
+interface TagsState {
+  nextAvailableId: number,
+  allTags: RtStatic<typeof Tags>,
+}
+
+const initialState: TagsState = {
+  nextAvailableId: 1,
+  allTags: [],
+};
+
+const tagsSlice = createSlice({
+  name: 'tags',
+  initialState,
+  reducers: {
+    setTags: (state, action) => ({
+      ...state,
+      allCards: action.payload,
+    }),
+    addTag: (state, action) => ({
+      ...state,
+      nextAvailableId: state.nextAvailableId + 1,
+      allTags: state.allTags.concat({
+        id: `tag-${state.nextAvailableId}`,
+        label: action.payload.label,
+        color: action.payload.color,
+      }),
+    }),
+    resetTags: () => initialState,
+  },
+});
+
+export const {
+  setTags,
+  addTag,
+  resetTags,
+} = tagsSlice.actions;
