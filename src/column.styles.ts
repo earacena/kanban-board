bd39e314123eda9f5a5cdfa930e38bd942896e14
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-04-10 21:16:23 -0400

    Implement style for column delete button

--- src/column.styles.ts
+++ src/column.styles.ts
@@ -83,3 +83,17 @@ export const cardFormButtonStyle = css({
     transform: 'translateY(2px)',
   },
 });
+
+export const columnDeleteButtonStyle = css({
+  border: '1px lightgrey solid',
+  color: 'red',
+  margin: '0.5rem',
+  borderRadius: '15%',
+  height: '2rem',
+  boxShadow: '0px 3px 10px rgb(0, 0, 0, 0.1)',
+  '&:hover': {
+    backgroundColor: 'lightgray',
+    transform: 'translateY(-2px)',
+    boxShadow: '0px 3px 10px rgb(0, 0, 0, 0.2)',
+  },
+});
