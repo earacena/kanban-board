5e2ea36c548a9e0da28ce32de528182027130b50
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-04-12 23:01:57 -0400

    Implement component for editing cards

---
+++ src/CardEditForm.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from '@emotion/react';
+import { Modal, Textarea } from '@mantine/core';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { useAppDispatch } from './hooks';
+import { updateCardBriefBody } from './cards.slice';
+
+interface CardEditFormProps {
+  id: string,
+  cardBrief: string;
+  cardBody: string;
+  cardEditFormOpened: boolean;
+  setCardEditFormOpened: (value: React.SetStateAction<boolean>) => void;
+}
+
+type Inputs = {
+  brief: string,
+  body: string,
+};
+
+function CardEditForm({
+  id,
+  cardBrief,
+  cardBody,
+  cardEditFormOpened,
+  setCardEditFormOpened,
+}: CardEditFormProps) {
+  const dispatch = useAppDispatch();
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<Inputs>({
+    defaultValues: {
+      brief: cardBrief,
+      body: cardBody,
+    },
+  });
+
+  const onSubmit: SubmitHandler<Inputs> = ({ brief, body }) => {
+    dispatch(updateCardBriefBody({ id, newBrief: brief, newBody: body }));
+
+    reset({
+      brief: '',
+      body: '',
+    });
+
+    setCardEditFormOpened(false);
+  };
+
+  return (
+    <Modal
+      opened={cardEditFormOpened}
+      onClose={() => setCardEditFormOpened(false)}
+      title="Edit card"
+    >
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Textarea
+          css={{ borderBottom: '1px lightgray solid', marginBottom: '0.5rem' }}
+          placeholder="Write a brief sentence about the task..."
+          label="Brief description"
+          aria-label="card-edit-brief-textarea"
+          error={errors.brief?.type === 'required' ? 'Cards must have a brief description' : null}
+          {...register('brief', { required: true })}
+          maxRows={2}
+        />
+
+        <Textarea
+          placeholder="Write about the task in more detail..."
+          label="Detailed description"
+          aria-label="card-edit-body-textarea"
+          {...register('body')}
+          autosize
+          minRows={4}
+        />
+        <button type="submit">Update</button>
+      </form>
+    </Modal>
+  );
+}
+
+export default CardEditForm;
