9d04437419c21b9e58cb66cf20b09939007e72c5
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-05-16 21:53:33 -0400

    Re-export settings button style

--- src/features/Settings/index.ts
+++ src/features/Settings/index.ts
@@ -1,2 +1,4 @@
 export { default as Settings } from './Settings';
 export { default as settingsReducer } from './stores/settings.slice';
+
+export { default as openSettingsButtonStyle } from './styles/settings.styles';
