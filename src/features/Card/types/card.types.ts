adad3fe1601c4908e10aacc0781042f836e50824
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-02-13 16:33:31 -0500

    Make card's body, color, and tags optional props

--- src/features/Card/types/card.types.ts
+++ src/features/Card/types/card.types.ts
@@ -2,21 +2,22 @@ import {
   String as RtString,
   Record as RtRecord,
   Array as RtArray,
+  Optional as RtOptional,
 } from 'runtypes';
 
 export const CardType = RtRecord({
   id: RtString,
   columnId: RtString,
   brief: RtString,
-  body: RtString,
-  color: RtString,
-  tags: RtArray(
+  body: RtOptional(RtString),
+  color: RtOptional(RtString),
+  tags: RtOptional(RtArray(
     RtRecord({
       id: RtString,
       label: RtString,
       color: RtString,
     }),
-  ),
+  )),
 });
 
 export const Cards = RtArray(CardType);
