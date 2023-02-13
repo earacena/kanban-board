ce5c62c2ab4744f4a2adf0461bc81bcbbe85d619
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-02-13 16:32:25 -0500

    Remove shadow and hover effects for cardEditButton

--- src/features/Card/styles/cardEditForm.styles.ts
+++ src/features/Card/styles/cardEditForm.styles.ts
@@ -6,12 +6,6 @@ export const cardEditButtonStyle = css({
   marginBottom: '0.5rem',
   borderRadius: '15%',
   height: '2rem',
-  boxShadow: '0px 3px 10px rgb(0, 0, 0, 0.1)',
-  '&:hover': {
-    backgroundColor: 'lightgray',
-    transform: 'translateY(-2px)',
-    boxShadow: '0px 3px 10px rgb(0, 0, 0, 0.2)',
-  },
 });
 
 export const cardEditFormStyle = css({
