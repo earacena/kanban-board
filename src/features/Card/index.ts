b826a41aae9530b34ecb1f442a44beef9686d17d
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-02-17 14:27:31 -0500

    Remove CardEditForm

--- src/features/Card/index.ts
+++ src/features/Card/index.ts
@@ -1,6 +1,5 @@
 export { default as Card } from './Card';
 export { default as CardForm } from './CardForm';
-export { default as CardEditForm } from './CardEditForm';
 export { default as ExpandedCard } from './ExpandedCard';
 export { default as cardsReducer } from './stores/cards.slice';
 export * from './stores/cards.slice';
