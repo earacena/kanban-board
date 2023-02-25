827329a167c36d9c9e6b290778f0728ca00dba3d
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-02-24 23:42:53 -0500

    Restructure and reimplement TagPicker

--- src/features/Tag/TagPicker.tsx
+++ src/features/Tag/TagPicker.tsx
@@ -1,58 +1,111 @@
-import React from 'react';
+import React, { useState } from 'react';
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx } from '@emotion/react';
-import { Badge } from '@mantine/core';
-import { IoMdCheckmark } from 'react-icons/io';
-import { tagStyle, tagPickerStyle } from './styles/tagPicker.styles';
-import type { Tags } from './types/tag.types';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import {
+  Button, Chip, ColorPicker, Popover, createStyles,
+} from '@mantine/core';
+import { tagPickerStyle } from './styles/tagPicker.styles';
+import type { Tags as TagsType } from './types/tag.types';
+import { addTag } from './stores/tag.slice';
+import { useAppDispatch } from '../../hooks';
 
 type TagPickerProps = {
-  tags: Tags;
-  appliedTags: Tags;
-  setAppliedTags: (value: React.SetStateAction<Tags>) => void;
+  tags: TagsType;
+  appliedTags: TagsType | undefined,
+  updateCardTags: (updatedTags: TagsType) => void,
 };
 
-function TagPicker({ tags, appliedTags, setAppliedTags }: TagPickerProps) {
+type TagInputs = {
+  tagLabel: string,
+};
+
+const useStyles = createStyles((theme, _params, getRef) => ({
+  label: {
+    '&[data-checked]': {
+      '&, &:hover': {
+        backgroundColor: theme.colors.blue[theme.fn.primaryShade()],
+        color: theme.white,
+      },
+
+      [`& .${getRef('iconWrapper')}`]: {
+        color: theme.white,
+      },
+    },
+  },
+
+  iconWrapper: {
+    ref: getRef('iconWrapper'),
+  },
+}));
+
+function TagPicker({
+  tags,
+  appliedTags,
+  updateCardTags,
+}: TagPickerProps) {
+  const dispatch = useAppDispatch();
+  const { classes } = useStyles();
+  const [tagColor, setTagColor] = useState<string>('rgba(0,0,0,0)');
+
+  const { register, handleSubmit } = useForm<TagInputs>({
+    defaultValues: {
+      tagLabel: '',
+    },
+  });
+
+  const onSubmit: SubmitHandler<TagInputs> = ({ tagLabel }) => {
+    dispatch(addTag({ label: tagLabel, color: tagColor }));
+  };
+
   return (
     <div css={tagPickerStyle}>
       {tags.map((tag) => (
-        appliedTags.find((t) => t.id === tag.id)
-          ? (
-            <Badge
-              component="button"
-              color={tag.color}
-              css={tagStyle}
-              key={tag.id}
-              type="button"
-              variant="outline"
-              onClick={() => (
-                appliedTags.find((t) => t.id === tag.id)
-                  ? setAppliedTags(appliedTags.filter((t) => t.id !== tag.id))
-                  : setAppliedTags(appliedTags.concat(tag))
-              )}
-            >
-              {tag.label}
-              <IoMdCheckmark size={10} />
-            </Badge>
-          ) : (
-            <Badge
-              component="button"
-              color={tag.color}
-              css={tagStyle}
-              key={tag.id}
-              type="button"
-              variant="outline"
-              onClick={() => (
-                appliedTags.find((t) => t.id === tag.id)
-                  ? setAppliedTags(appliedTags.filter((t) => t.id !== tag.id))
-                  : setAppliedTags(appliedTags.concat(tag))
-              )}
-            >
-              {tag.label}
-            </Badge>
-          )
+        <Chip
+          classNames={classes}
+          color={tag.color}
+          key={tag.id}
+          radius="sm"
+          size="lg"
+          onClick={() => (
+            appliedTags?.find((t) => t.id === tag.id)
+              ? updateCardTags(appliedTags.filter((t) => t.id !== tag.id))
+              : updateCardTags(appliedTags?.concat(tag) ?? [tag])
+          )}
+          checked={appliedTags?.find((t) => t.id === tag.id) !== undefined}
+        >
+          {tag.label}
+        </Chip>
       ))}
+      {
+        tags.length === 0 && 'No existing tags'
+      }
+      <Popover trapFocus shadow="md">
+        <Popover.Target>
+          <Button css={{ marginLeft: '10px' }} radius="xl" variant="subtle">
+            Add
+          </Button>
+        </Popover.Target>
+        <Popover.Dropdown>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input
+              aria-label="tag label"
+              {...register('tagLabel')}
+            />
+            <ColorPicker
+              css={{ marginTop: '10px' }}
+              aria-label="tag color picker"
+              value={tagColor}
+              onChange={(color) => setTagColor(color)}
+            />
+
+            <Button type="submit" radius="xl">
+              Create Tag
+            </Button>
+          </form>
+        </Popover.Dropdown>
+      </Popover>
     </div>
   );
 }
