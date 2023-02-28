f72180fe7783c8368abf781dda879cd7fc9ad21d
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-02-27 21:41:33 -0500

    Comment out sections not implemented

--- src/features/Board/Boards.tsx
+++ src/features/Board/Boards.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { useAppSelector } from '../../hooks';
-import Board from './Board';
+// import { useAppSelector } from '../../hooks';
+// import Board from './Board';
 
 function Boards() {
-  const allBoards = useAppSelector((state) => state.boards.allBoards);
+  // const allBoards = useAppSelector((state) => state.boards.allBoards);
 
   return (
     <div>
       {
-        allBoards.map((b) => <Board id={b.id} />)
+        // allBoards.map((b) => <Board id={b.id} />)
       }
     </div>
   );
