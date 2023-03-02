c732b992cb8b7e844e23508b8a4264f18e9598be
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-03-01 21:11:04 -0500

    Make Board a clickable component

--- src/features/Board/Board.tsx
+++ src/features/Board/Board.tsx
@@ -1,4 +1,8 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from '@emotion/react';
 import React from 'react';
+import { Button, Text } from '@mantine/core';
 import { Board as BoardType } from './types/board.types';
 
 type BoardProps = {
@@ -7,9 +11,21 @@ type BoardProps = {
 
 function Board({ board }: BoardProps) {
   return (
-    <div>
-      {board.label}
-    </div>
+    <Button
+      style={{
+        border: '1px lightgrey solid',
+        margin: '4px',
+        boxShadow: '0px 3px 10px rgb(0, 0, 0, 0.2)',
+        width: '300px',
+        borderRadius: '4px',
+      } as React.CSSProperties}
+      variant="light"
+      size="xl"
+    >
+      <Text truncate>
+        {board.label}
+      </Text>
+    </Button>
   );
 }
 
