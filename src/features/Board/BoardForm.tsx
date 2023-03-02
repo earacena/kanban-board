937fb4ae61153e3afd433d5ce0420aff9a895c31
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-03-01 21:10:50 -0500

    Implement BoardForm

---
+++ src/features/Board/BoardForm.tsx
@@ -0,0 +1,85 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from '@emotion/react';
+import React, { SetStateAction } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { Button, Text } from '@mantine/core';
+import { BsPlus } from 'react-icons/bs';
+import { FcCancel } from 'react-icons/fc';
+import { cardFormButtonStyle, sortableItemStyle } from '../Column/styles/column.styles';
+import { useAppDispatch } from '../../hooks';
+import { addBoard } from './stores/boards.slice';
+
+type Inputs = {
+  label: string;
+};
+
+type BoardFormProps = {
+  setBoardFormOpened: (value: SetStateAction<boolean>) => void;
+};
+
+function BoardForm({ setBoardFormOpened }: BoardFormProps) {
+  const dispatch = useAppDispatch();
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<Inputs>({
+    defaultValues: {
+      label: '',
+    },
+  });
+
+  const onSubmit: SubmitHandler<Inputs> = (formData) => {
+    const { label } = formData;
+
+    dispatch(
+      addBoard({
+        label,
+      }),
+    );
+
+    reset({
+      label: '',
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <textarea
+        style={{
+          ...sortableItemStyle,
+          fontFamily: 'Open Sans',
+          flexGrow: 1,
+          resize: 'none',
+        } as React.CSSProperties}
+        aria-label="board label"
+        {...register('label', { required: true })}
+      />
+      <Text color="red" size={14}>
+        {errors.label?.type === 'required'
+          ? 'Boards must have a label'
+          : null}
+      </Text>
+      <Button
+        css={cardFormButtonStyle}
+        type="submit"
+        leftIcon={<BsPlus size={19} />}
+      >
+        ADD NEW CARD
+      </Button>
+      <Button
+        variant="subtle"
+        color="red"
+        leftIcon={<FcCancel size={19} />}
+        onClick={() => setBoardFormOpened(false)}
+      >
+        CANCEL
+      </Button>
+    </form>
+  );
+}
+
+export default BoardForm;
