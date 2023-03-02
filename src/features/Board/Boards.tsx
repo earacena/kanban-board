8ce11c466e3f9d21500d8fc47057affe300ed06a
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-03-01 21:12:00 -0500

    Integrate BoardForm and improve style of Boards

--- src/features/Board/Boards.tsx
+++ src/features/Board/Boards.tsx
@@ -1,20 +1,43 @@
-import React from 'react';
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from '@emotion/react';
+import React, { useState } from 'react';
+import { Button } from '@mantine/core';
+import { BsPlus } from 'react-icons/bs';
 import { useAppSelector } from '../../hooks';
 import Board from './Board';
+import BoardForm from './BoardForm';
+import { cardFormButtonLabelStyle, cardFormButtonStyle } from '../Column/styles/column.styles';
 
 function Boards() {
   const allBoards = useAppSelector((state) => state.boards.allBoards);
+  const [boardFormOpened, setBoardFormOpened] = useState<boolean>(false);
 
   return (
-    <ul>
-      {
-        allBoards.map((b) => (
-          <li key={b.id}>
-            <Board board={b} />
-          </li>
-        ))
-      }
-    </ul>
+    <div css={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <ul css={{ padding: 0 }}>
+        {
+          allBoards.map((b) => (
+            <li key={b.id} css={{ listStyle: 'none' }}>
+              <Board board={b} />
+            </li>
+          ))
+        }
+      </ul>
+      {boardFormOpened && <BoardForm setBoardFormOpened={setBoardFormOpened} />}
+      {!boardFormOpened && (
+        <Button
+          css={cardFormButtonStyle}
+          type="button"
+          variant="subtle"
+          onClick={() => setBoardFormOpened(true)}
+        >
+          <BsPlus size={19} />
+          <span css={cardFormButtonLabelStyle}>ADD NEW BOARD</span>
+        </Button>
+      )}
+
+    </div>
   );
 }
 
