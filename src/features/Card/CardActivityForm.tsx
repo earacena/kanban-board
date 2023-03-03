711fb94a98bedf5d375903a40e0bc7a65d52111f
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-03-03 03:14:30 -0500

    Implement CardActivityForm

---
+++ src/features/Card/CardActivityForm.tsx
@@ -0,0 +1,79 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import React from 'react';
+import { jsx } from '@emotion/react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import {
+  Button, Stack, Text,
+} from '@mantine/core';
+import { sortableItemStyle } from '../Column/styles/column.styles';
+import { useAppDispatch } from '../../hooks';
+import { addCardActivity } from './stores/cards.slice';
+
+type Inputs = {
+  content: string;
+};
+
+type CardActivityProps = {
+  cardId: string;
+};
+
+function CardActivityForm({ cardId }: CardActivityProps) {
+  const dispatch = useAppDispatch();
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<Inputs>({
+    defaultValues: {
+      content: '',
+    },
+  });
+
+  const onSubmit: SubmitHandler<Inputs> = ({ content }) => {
+    dispatch(
+      addCardActivity({
+        cardId,
+        type: 'contribution',
+        content,
+      }),
+    );
+
+    reset({
+      content: '',
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Stack>
+        <textarea
+          style={
+            {
+              ...sortableItemStyle,
+              fontFamily: 'Open Sans',
+              flexGrow: 1,
+              resize: 'none',
+              marginBottom: '0',
+            } as React.CSSProperties
+          }
+          aria-label="activity content"
+          placeholder="How have you contributed towards this task?"
+          {...register('content', { required: true })}
+        />
+        <Text size="sm" color="red" align="center" css={{ marginTop: '0' }}>
+          {errors.content?.type === 'required'
+            ? 'Activity posts must have content'
+            : null}
+        </Text>
+      </Stack>
+      <Button radius="xl" type="submit">
+        Post
+      </Button>
+    </form>
+  );
+}
+
+export default CardActivityForm;
