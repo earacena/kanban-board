d0daa3393eb7fa98cde2c351c0c44b3cf9ae766a
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-03-04 20:40:30 -0500

    Implement button for deleting tags

--- src/features/Tag/TagPickerForm.tsx
+++ src/features/Tag/TagPickerForm.tsx
@@ -4,33 +4,39 @@ import React, { useState } from 'react';
 import { jsx } from '@emotion/react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { IoMdCheckmark } from 'react-icons/io';
+import { TiDeleteOutline } from 'react-icons/ti';
 import {
-  Button, ColorPicker, Group, Popover, Text,
+  ActionIcon,
+  Button,
+  ColorPicker,
+  Group,
+  Popover,
+  Text,
 } from '@mantine/core';
 import { tagPickerStyle } from './styles/tagPicker.styles';
 import type { Tags as TagsType } from './types/tag.types';
-import { addTag } from './stores/tag.slice';
+import { addTag, removeTag } from './stores/tag.slice';
 import { useAppDispatch } from '../../hooks';
 
 type TagPickerProps = {
   tags: TagsType;
-  appliedTags: TagsType | undefined,
-  updateCardTags: (updatedTags: TagsType) => void,
-  removeTagFromCards: (tagId: string) => void,
+  appliedTags: TagsType | undefined;
+  updateCardTags: (updatedTags: TagsType) => void;
+  removeTagFromCards: (tagId: string) => void;
 };
 
 type TagInputs = {
-  tagLabel: string,
+  tagLabel: string;
 };
 
 function TagPicker({
   tags,
   appliedTags,
   updateCardTags,
-//  removeTagFromCards,
+  removeTagFromCards,
 }: TagPickerProps) {
   const dispatch = useAppDispatch();
-  const [tagColor, setTagColor] = useState<string>('rgba(0,0,0,0)');
+  const [tagColor, setTagColor] = useState<string>('rgba(46, 130, 232, 1)');
 
   const { register, handleSubmit } = useForm<TagInputs>({
     defaultValues: {
@@ -42,10 +48,10 @@ function TagPicker({
     dispatch(addTag({ label: tagLabel, color: tagColor }));
   };
 
-  // const handleRemoveTag = (tagId: string) => {
-  //   dispatch(removeTag({ tagId }));
-  //   removeTagFromCards(tagId);
-  // };
+  const handleRemoveTag = (tagId: string) => {
+    dispatch(removeTag({ tagId }));
+    removeTagFromCards(tagId);
+  };
 
   const tagIsChecked = (id: string): boolean => appliedTags?.find((t) => t.id === id) !== undefined;
 
@@ -54,32 +60,41 @@ function TagPicker({
       {tags.map((tag) => {
         const checked: boolean = tagIsChecked(tag.id);
         return (
-          <Button
-            key={tag.id}
-            type="button"
-            onClick={() => (
-              appliedTags?.find((t) => t.id === tag.id)
-                ? updateCardTags(appliedTags.filter((t) => t.id !== tag.id))
-                : updateCardTags(appliedTags?.concat(tag) ?? [tag])
-            )}
-            variant={checked ? 'gradient' : 'outline'}
-            gradient={{ from: tag.color, to: tag.color }}
-            css={{ marginRight: '10px' }}
+          <div
+            css={{
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+            }}
           >
-            <Group>
-              {checked && <IoMdCheckmark size={20} css={{ padding: '0' }} />}
-              <Text fw={800} size="lg">
-                {tag.label}
-              </Text>
-            </Group>
-          </Button>
+            <ActionIcon onClick={() => handleRemoveTag(tag.id)} color="red">
+              <TiDeleteOutline />
+            </ActionIcon>
+            <Button
+              key={tag.id}
+              type="button"
+              onClick={() => (appliedTags?.find((t) => t.id === tag.id)
+                ? updateCardTags(appliedTags.filter((t) => t.id !== tag.id))
+                : updateCardTags(appliedTags?.concat(tag) ?? [tag]))}
+              variant={checked ? 'gradient' : 'outline'}
+              gradient={{ from: tag.color, to: tag.color }}
+              css={{ marginRight: '10px' }}
+            >
+              <Group>
+                {checked && <IoMdCheckmark size={20} css={{ padding: '0' }} />}
+                <Text fw={800} size="lg">
+                  {tag.label}
+                </Text>
+              </Group>
+            </Button>
+          </div>
         );
       })}
-      <Text fw={300} color="dimmed">
-        {
-          tags.length === 0 && 'No existing tags'
-        }
-      </Text>
+      {tags.length === 0 && (
+        <Text fw={300} color="dimmed">
+          No existing tags
+        </Text>
+      )}
       <Popover trapFocus shadow="md">
         <Popover.Target>
           <Button css={{ marginLeft: '10px' }} radius="xl" variant="subtle">
@@ -87,7 +102,14 @@ function TagPicker({
           </Button>
         </Popover.Target>
         <Popover.Dropdown>
-          <form onSubmit={handleSubmit(onSubmit)} css={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            css={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
             <input
               aria-label="tag label"
               css={{
