c9110fd4a1cdbd72d4739051cde78ae386707038
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-03-13 21:43:01 -0400

    Implement slice for handling user auth and session

---
+++ src/features/Auth/stores/auth.slice.ts
@@ -0,0 +1,53 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface UserSessionInfo {
+  id: number,
+  name: string,
+  username: string,
+}
+
+interface AuthState {
+  user: UserSessionInfo | undefined,
+  isFetching: boolean,
+}
+
+interface SetUserPayload {
+  user: UserSessionInfo,
+}
+
+interface SetIsFetchingPayload {
+  isFetching: boolean,
+}
+
+const initialState: AuthState = {
+  user: undefined,
+  isFetching: false,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    setUser: (
+      state: AuthState,
+      action: PayloadAction<SetUserPayload>,
+    ) => ({
+      ...state,
+      user: action.payload.user,
+    }),
+    setIsFetching: (
+      state: AuthState,
+      action: PayloadAction<SetIsFetchingPayload>,
+    ) => ({
+      ...state,
+      isFetching: action.payload.isFetching,
+    }),
+  },
+});
+
+export const {
+  setUser,
+  setIsFetching,
+} = authSlice.actions;
+
+export default authSlice.reducer;
