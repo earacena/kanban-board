e0ff41d9bebedc8b08970aa407f7bb3ca5a8ade0
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-03-14 21:08:57 -0400

    Implement service for logging in

---
+++ src/services/login.service.ts
@@ -0,0 +1,32 @@
+import decodeWith from '../util/decode';
+import { UserSessionInfoType } from '../features/Auth/types/auth.types';
+import type { UserSessionInfo } from '../features/Auth/types/auth.types';
+
+interface LoginProps {
+  username: string;
+  password: string;
+}
+
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
+
+const login = async ({ username, password }: LoginProps) => {
+  const response = await fetch(`${backendUrl}/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ username, password }),
+  });
+
+  const responseJson = await response.json();
+  if (responseJson.error) {
+    throw new Error(responseJson.error);
+  } else {
+    const userSessionInfo: UserSessionInfo = decodeWith(UserSessionInfoType)(responseJson);
+    return userSessionInfo;
+  }
+};
+
+export default {
+  login,
+};
