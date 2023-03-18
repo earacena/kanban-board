3f3823b0f2ea9eb51d76564c11a5faed2c92fb77
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-03-17 21:26:50 -0400

    Implement UserCard for displaying user details

--- src/features/User/UserCard.tsx
+++ src/features/User/UserCard.tsx
@@ -1,10 +1,40 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from '@emotion/react';
 import React from 'react';
+import {
+  Avatar,
+  Group,
+  Text,
+} from '@mantine/core';
+import { useAppSelector } from '../../hooks';
 
 function UserCard() {
+  const userSession = useAppSelector((state) => state.auth.user);
+
   return (
-    <div>
-      User
-    </div>
+    <button
+      type="button"
+      css={{
+        backgroundColor: 'transparent',
+        border: 'none',
+        borderRadius: '10px',
+        '&:hover': {
+          backgroundColor: 'lightgray',
+          cursor: 'pointer',
+        },
+      }}
+    >
+      <Group css={{ padding: '10px' }}>
+        <Avatar radius="xl" size="lg" color="dark" />
+        <Text size="lg" fw={400}>
+          {userSession ? userSession?.name : 'Please sign in.'}
+        </Text>
+        <Text size="sm" fw={300} color="dimmed">
+          {userSession && userSession?.name}
+        </Text>
+      </Group>
+    </button>
   );
 }
 
