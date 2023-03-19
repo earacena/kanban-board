97876905ff118f92d95a578b2d8b01ebc54434a8
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-03-18 21:43:48 -0400

    Improve style and layout of LoginForm

--- src/features/Login/LoginForm.tsx
+++ src/features/Login/LoginForm.tsx
@@ -1,4 +1,9 @@
-import { Button, Text, TextInput } from '@mantine/core';
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from '@emotion/react';
+import {
+  Button, Divider, Stack, Text, TextInput,
+} from '@mantine/core';
 import React, { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
@@ -51,32 +56,73 @@ function LoginForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <TextInput
-        id="username-input"
-        aria-label="username"
-        {...register('username', { required: true })}
-        error={
-          errors.username?.type === 'required' && (
-            <Text color="red" size="sm">
-              Please enter a username
-            </Text>
-          )
-        }
-      />
-      <TextInput
-        id="password-input"
-        aria-label="password"
-        error={
-          errors.username?.type === 'required' && (
-            <Text color="red" size="sm">
-              Please enter a username
-            </Text>
-          )
-        }
-        {...register('password', { required: true })}
-      />
-      <Button type="submit" loading={loading} />
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      css={{
+        display: 'flex',
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Stack
+        css={{
+          backgroundColor: 'white',
+          padding: '32px',
+          borderRadius: '16px',
+        }}
+      >
+        <TextInput
+          id="username-input"
+          label="Username"
+          aria-label="username"
+          {...register('username', { required: true })}
+          error={
+            errors.username?.type === 'required' ? (
+              <Text color="red" size="sm">
+                Please enter a username
+              </Text>
+            ) : (
+              false
+            )
+          }
+          size="lg"
+          radius="md"
+        />
+        <TextInput
+          id="password-input"
+          label="Password"
+          aria-label="password"
+          type="password"
+          error={
+            errors.password?.type === 'required' ? (
+              <Text color="red" size="sm">
+                Please enter a password
+              </Text>
+            ) : (
+              false
+            )
+          }
+          {...register('password', { required: true })}
+          size="lg"
+          radius="md"
+        />
+        <Button type="submit" loading={loading} size="lg">
+          Sign In
+        </Button>
+        <Divider
+          size="xs"
+          label="Don't have an account?"
+          labelPosition="center"
+        />
+        <Button
+          size="lg"
+          variant="outline"
+          onClick={() => navigate('/register')}
+        >
+          Register
+        </Button>
+      </Stack>
     </form>
   );
 }
