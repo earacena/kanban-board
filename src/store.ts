5b9cdcd077412dea18a885bd1c1e1551c4b7977b
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-03-28 15:11:58 -0400

    Disable automatic state storing in localStorage

--- src/store.ts
+++ src/store.ts
@@ -1,12 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import throttle from 'lodash/throttle';
+// import throttle from 'lodash/throttle';
 import { cardsReducer } from './features/Card';
 import { columnsReducer } from './features/Column';
 import { tagsReducer } from './features/Tag';
 import { settingsReducer } from './features/Settings';
 import { boardsReducer } from './features/Board';
 import { authReducer } from './features/Auth';
-import { loadState, saveState } from './features/App/localStorage';
+// import { saveState } from './features/App/localStorage';
 
 const store = configureStore({
   reducer: {
@@ -17,13 +17,13 @@ const store = configureStore({
     tags: tagsReducer,
     settings: settingsReducer,
   },
-  preloadedState: loadState(),
+// preloadedState: loadState(),
 });
 
 // Save changes to state in browser
-store.subscribe(throttle(() => {
-  saveState(store.getState());
-}, 1000));
+// store.subscribe(throttle(() => {
+//   saveState(store.getState());
+// }, 1000));
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
