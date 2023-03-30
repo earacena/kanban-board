adbece8035e190c1e4a8b39337740c2862632597
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-03-30 00:45:19 -0400

    Implement button for deleting boards

--- src/features/Settings/Settings.tsx
+++ src/features/Settings/Settings.tsx
@@ -6,22 +6,43 @@ import {
   Button,
   ColorInput,
   Modal,
+  Stack,
   Text,
 } from '@mantine/core';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { setThemeColor } from './stores/settings.slice';
+import { removeBoard, setSelectedBoard } from '../Board/stores/boards.slice';
+import { removeCardsWithColumnId } from '../Card';
 
 interface SettingsProps {
   settingsOpened: boolean,
   setSettingsOpened: (value: React.SetStateAction<boolean>) => void,
 }
 
-function Settings({ settingsOpened, setSettingsOpened }: SettingsProps) {
+function Settings({
+  settingsOpened,
+  setSettingsOpened,
+}: SettingsProps) {
   const dispatch = useAppDispatch();
   const { themeColor } = useAppSelector((state) => state.settings);
+  const selectedBoardId = useAppSelector((state) => state.boards.selectedBoardId);
+  const boards = useAppSelector((state) => state.boards.allBoards);
+  const selectedBoard = boards.find((b) => b.id === selectedBoardId);
   const [color, setColor] = useState(themeColor);
+  const columns = useAppSelector((state) => state.columns.allColumns);
+  const columnsInBoard = columns.filter((c) => c.boardId === selectedBoardId);
 
   const handleSetColor = () => dispatch(setThemeColor({ themeColor: color }));
+
+  const handleDeleteBoard = () => {
+    columnsInBoard.forEach((c) => {
+      dispatch(removeCardsWithColumnId({ columnId: c.id }));
+    });
+
+    dispatch(removeBoard({ boardId: selectedBoardId }));
+    setSettingsOpened(false);
+    dispatch(setSelectedBoard({ boardId: '' }));
+  };
 
   return (
     <Modal
@@ -29,18 +50,22 @@ function Settings({ settingsOpened, setSettingsOpened }: SettingsProps) {
       onClose={() => setSettingsOpened(false)}
       title="Settings"
     >
-      <div css={{ display: 'flex', flexDirection: 'column' }}>
-        <Text size="sm" weight={500}>
-          Background color
-        </Text>
-        <ColorInput
-          css={{ marginBottom: '1rem' }}
-          format="hex"
-          value={color}
-          onChange={setColor}
-        />
-        <Button type="button" onClick={handleSetColor}>Save color</Button>
-      </div>
+      <Stack align="center">
+        <div css={{ display: 'flex', flexDirection: 'column' }}>
+          <Text size="sm" weight={500}>
+            Background color
+          </Text>
+          <ColorInput
+            css={{ marginBottom: '1rem' }}
+            format="hex"
+            value={color}
+            onChange={setColor}
+          />
+          <Button type="button" onClick={handleSetColor}>Save color</Button>
+
+        </div>
+        <Button variant="subtle" color="red" onClick={handleDeleteBoard}>{`Delete '${selectedBoard?.label}'`}</Button>
+      </Stack>
     </Modal>
   );
 }
