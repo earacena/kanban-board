a76cb1701a7148bf961e7806f52f4272f57c8edf
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-06-09 23:43:20 -0400

    feat(card): add card service functions for API

---
+++ src/services/card.service.ts
@@ -0,0 +1,162 @@
+import { ErrorResponse, CardResponse, CardsResponse } from "./common.types";
+
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
+
+interface CreateProps {
+  userId: string,
+  columnId: string,
+  brief: string,
+  body: string,
+}
+
+interface FetchCardProps {
+  cardId: string,
+}
+
+interface FetchCardsOfColumnProps {
+  columnId: string,
+}
+
+interface UpdateProps {
+  cardId: string,
+  changes: {
+    columnId: string,
+    brief: string,
+    body: string,
+    color: string,
+  }
+}
+
+interface DeleteCardProps {
+  cardId: string,
+}
+
+interface DeleteCardsOfColumnProps {
+  columnId: string,
+}
+
+export const create = async ({
+  userId,
+  columnId,
+  brief,
+  body,
+}: CreateProps) => {
+  const response = await fetch(`${backendUrl}/api/cards/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+    body: JSON.stringify({
+      userId,
+      columnId,
+      brief,
+      body,
+    }),
+  });
+
+  const responseJson = await response.json();
+  if (!responseJson.success) {
+    const errorResponse = ErrorResponse.parse(responseJson);
+    const errorMessages = errorResponse.errors?.map((err) => err.message);
+    throw new Error(errorMessages?.join(' '));
+  } else {
+    const newCard = CardResponse.parse(responseJson).data.card;
+    return newCard;
+  }
+};
+
+export const fetchCardById = async ({ cardId }: FetchCardProps) => {
+  const response = await fetch(`${backendUrl}/api/cards/${cardId}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+  });
+
+  const responseJson = await response.json();
+  if (!responseJson.success) {
+    const errorResponse = ErrorResponse.parse(responseJson);
+    const errorMessages = errorResponse.errors?.map((err) => err.message);
+    throw new Error(errorMessages?.join(' '));
+  } else {
+    const fetchedCard = CardResponse.parse(responseJson).data.card;
+    return fetchedCard;
+  }
+};
+
+export const fetchCardsByColumnId = async ({ columnId }: FetchCardsOfColumnProps) => {
+  const response = await fetch(`${backendUrl}/api/cards/column/${columnId}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+  });
+
+  const responseJson = await response.json();
+  if (!responseJson.success) {
+    const errorResponse = ErrorResponse.parse(responseJson);
+    const errorMessages = errorResponse.errors?.map((err) => err.message);
+    throw new Error(errorMessages?.join(' '));
+  } else {
+    const fetchedCards = CardsResponse.parse(responseJson).data.cards;
+    return fetchedCards;
+  }
+};
+
+export const update = async ({ cardId, changes }: UpdateProps) => {
+  const response = await fetch(`${backendUrl}/api/cards/${cardId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+    body: JSON.stringify(changes),
+  });
+
+  const responseJson = await response.json();
+  if (!responseJson.success) {
+    const errorResponse = ErrorResponse.parse(responseJson);
+    const errorMessages = errorResponse.errors?.map((err) => err.message);
+    throw new Error(errorMessages?.join(' '));
+  } else {
+    const updatedCard = CardResponse.parse(responseJson).data.card;
+    return updatedCard;
+  }
+};
+
+export const deleteCard = async ({ cardId }: DeleteCardProps) => {
+  const response = await fetch(`${backendUrl}/api/cards/${cardId}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+  });
+
+  const responseJson = await response.json();
+  if (!responseJson.success) {
+    const errorResponse = ErrorResponse.parse(responseJson);
+    const errorMessages = errorResponse.errors?.map((err) => err.message);
+    throw new Error(errorMessages?.join(' '));
+  }
+};
+
+export const deleteCardsByColumnId = async ({ columnId }: DeleteCardsOfColumnProps) => {
+  const response = await fetch(`${backendUrl}/api/cards/column/${columnId}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+  });
+
+  const responseJson = await response.json();
+  if (!responseJson.success) {
+    const errorResponse = ErrorResponse.parse(responseJson);
+    const errorMessages = errorResponse.errors?.map((err) => err.message);
+    throw new Error(errorMessages?.join(' '));
+  }
+};
