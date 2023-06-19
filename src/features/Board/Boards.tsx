f868242b4f043bf8c6fe4dfc18a00f39b4f1af83
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-06-18 22:16:57 -0400

    feat(board): implement fetching boards from API

--- src/features/Board/Boards.tsx
+++ src/features/Board/Boards.tsx
@@ -1,17 +1,40 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx } from '@emotion/react';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from '@mantine/core';
 import { BsPlus } from 'react-icons/bs';
-import { useAppSelector } from '../../hooks';
+import { useAppDispatch, useAppSelector } from '../../hooks';
 import Board from './Board';
 import BoardForm from './BoardForm';
 import { cardFormButtonLabelStyle, cardFormButtonStyle } from '../Column/styles/column.styles';
+import { ErrorType } from '../Login/types/registerForm.types';
+import logger from '../../util/Logger';
+import boardServices from '../../services/board.service';
+import { setBoards } from './stores/boards.slice';
 
 function Boards() {
+  const dispatch = useAppDispatch();
+  const session = useAppSelector((state) => state.auth.user);
+
   const allBoards = useAppSelector((state) => state.boards.allBoards);
   const [boardFormOpened, setBoardFormOpened] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchBoards = async () => {
+      try {
+        if (session) {
+          const fetchedBoards = await boardServices.fetchByUserId({ userId: session.id });
+          dispatch(setBoards({ allBoards: fetchedBoards }));
+        }
+      } catch (err: unknown) {
+        const decoded = ErrorType.parse(err);
+        logger.logError(decoded);
+      }
+    };
+
+    fetchBoards();
+  }, []);
 
   return (
     <div css={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
