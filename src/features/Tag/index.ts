ce07c5cb18f6d1f58923b42401a734bda0551044
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-06-19 19:45:53 -0400

    refactor(tag): update tag types and remove TagForm

--- src/features/Tag/index.ts
+++ src/features/Tag/index.ts
@@ -1,6 +1,5 @@
 export { default as Tags } from './Tags';
 export { default as TagPickerForm } from './TagPickerForm';
-export { default as TagForm } from './TagForm';
 export { default as tagsReducer } from './stores/tag.slice';
 
-export type { Tags as TagsType } from './types/tag.types';
+export type { TagType, TagArrayType } from './types/tag.types';
