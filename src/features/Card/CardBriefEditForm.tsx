f40e112530d47cf97df3ca1eb7028184d6fd4cb1
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-06-18 22:29:09 -0400

    feat(card): integrate API when updating card

--- src/features/Card/CardBriefEditForm.tsx
+++ src/features/Card/CardBriefEditForm.tsx
@@ -5,8 +5,11 @@ import React, { SetStateAction } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { ActionIcon, Text, TextInput } from '@mantine/core';
 import { BsCheck2, BsX } from 'react-icons/bs';
-import { useAppDispatch } from '../../hooks';
-import { updateCardBrief } from './stores/cards.slice';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { updateCard, updateCardBrief } from './stores/cards.slice';
+import { ErrorType } from '../Login/types/registerForm.types';
+import logger from '../../util/Logger';
+import cardServices from '../../services/card.service';
 
 type CardBriefEditFormProps = {
   cardId: string;
@@ -24,6 +27,9 @@ function CardBriefEditForm({
   setCardBriefEditFormOpened,
 }: CardBriefEditFormProps) {
   const dispatch = useAppDispatch();
+  const session = useAppSelector((state) => state.auth.user);
+  const allCards = useAppSelector((state) => state.cards.allCards);
+  const card = allCards.find((c) => c.id === cardId);
 
   const {
     register,
@@ -35,8 +41,25 @@ function CardBriefEditForm({
     },
   });
 
-  const onSubmit: SubmitHandler<Inputs> = (formData) => {
-    dispatch(updateCardBrief({ id: cardId, newBrief: formData.brief }));
+  const onSubmit: SubmitHandler<Inputs> = async (formData) => {
+    if (session && card) {
+      try {
+        const updatedCard = await cardServices.update({
+          cardId,
+          changes: {
+            ...card,
+            brief: formData.brief,
+          },
+        });
+        dispatch(updateCard({ updatedCard }));
+      } catch (err: unknown) {
+        const decoded = ErrorType.parse(err);
+        logger.logError(decoded);
+      }
+    } else {
+      dispatch(updateCardBrief({ id: cardId, newBrief: formData.brief }));
+    }
+
     setCardBriefEditFormOpened(false);
   };
 
