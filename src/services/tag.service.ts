8c658b6321f979372ca1ef9a64bc19fc74dc0595
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-06-26 23:34:28 -0400

    feat(tag): add services for cardIds operations

--- src/services/tag.service.ts
+++ src/services/tag.service.ts
@@ -15,6 +15,16 @@ interface DeleteTagProps {
   tagId: string,
 }
 
+interface AddCardIdToTagProps {
+  tagId: string,
+  cardId: string,
+}
+
+interface RemoveCardIdFromTagProps {
+  tagId: string,
+  cardId: string,
+}
+
 const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
 const create = async ({
@@ -85,10 +95,58 @@ const deleteTag = async ({ tagId }: DeleteTagProps) => {
   }
 };
 
+const addCardIdToTag = async ({ tagId, cardId }: AddCardIdToTagProps) => {
+  const response = await fetch(`${backendUrl}/api/tags/${tagId}/card`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      cardId,
+    }),
+    credentials: 'include',
+  });
+
+  const responseJson = await response.json();
+  if (!responseJson.success) {
+    const errorResponse = ErrorResponse.parse(responseJson);
+    const errorMessages = errorResponse.errors?.map((err) => err.message);
+    throw new Error(errorMessages?.join(' '));
+  } else {
+    const updatedTag = TagResponse.parse(responseJson).data.tag;
+    return updatedTag;
+  }
+};
+
+const removeCardIdFromTag = async ({ tagId, cardId }: RemoveCardIdFromTagProps) => {
+  const response = await fetch(`${backendUrl}/api/tags/${tagId}/card`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      cardId,
+    }),
+    credentials: 'include',
+  });
+
+  const responseJson = await response.json();
+  if (!responseJson.success) {
+    const errorResponse = ErrorResponse.parse(responseJson);
+    const errorMessages = errorResponse.errors?.map((err) => err.message);
+    throw new Error(errorMessages?.join(' '));
+  } else {
+    const updatedTag = TagResponse.parse(responseJson).data.tag;
+    return updatedTag;
+  }
+};
+
 const tagServices = {
   create,
   fetchUserTags,
   deleteTag,
+  addCardIdToTag,
+  removeCardIdFromTag,
 };
 
 export default tagServices;
